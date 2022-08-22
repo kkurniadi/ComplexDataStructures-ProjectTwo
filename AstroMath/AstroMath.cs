66ee116ff8e078dd41bb74fc2c6f3a4a2849ea73
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2022-08-22 14:13:20 +0800

    Added identifying comment

--- AstroMath/AstroMath.cs
+++ AstroMath/AstroMath.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+// Kirsten Kurniadi, ID: 30045816
+// Date: 22/08/2022
+// A 3rd party library that performs calculations
 namespace AstroMath
 {
     /// <summary>
