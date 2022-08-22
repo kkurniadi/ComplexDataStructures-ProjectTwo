6967bfdd037f1a33fb1602375d1fa45308e9bf0f
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2022-08-22 13:54:44 +0800

    Added XML comments

--- AstroMath/AstroMath.cs
+++ AstroMath/AstroMath.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace AstroMath
 {
+    /// <summary>
+    /// A library that performs calculation services for astronomical data.
+    /// </summary>
     public class AstroMath
     {
         /// <summary>
@@ -30,11 +33,21 @@ namespace AstroMath
             double distance = 1 / parallaxAngle;
             return distance;
         }
+        /// <summary>
+        /// Converts a temperature from Celsius to Kelvin.
+        /// </summary>
+        /// <param name="celsius">The temperature, in degrees Celsius.</param>
+        /// <returns>The temperature, in Kelvin.</returns>
         public double TempInKelvin(double celsius)
         {
             double kelvin = celsius + 273;
             return kelvin;
         }
+        /// <summary>
+        /// Calculates the distance from the centre of a black hole to the event horizon.
+        /// </summary>
+        /// <param name="mass">The mass of the black hole, in kilograms.</param>
+        /// <returns>The event horizon, in metres.</returns>
         public double EventHorizon(double mass)
         {
             double g = 6.674 * Math.Pow(10, -11);
