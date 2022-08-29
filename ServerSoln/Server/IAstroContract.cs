3da3a6b47b26491b2dca692c45cfdd818a1a0d02
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2022-08-29 12:48:55 +0800

    Add comments to server project files

--- ServerSoln/Server/IAstroContract.cs
+++ ServerSoln/Server/IAstroContract.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
-
+// Kirsten Kurniadi, ID: 30045816
+// Date: 29/08/2022
+// An interface that references the third-party library
 namespace Server
 {
     internal interface IAstroContract
