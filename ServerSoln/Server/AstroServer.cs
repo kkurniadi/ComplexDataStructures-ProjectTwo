0ec369d502879b121d030651d0f3c4c15e8c186e
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2022-08-29 12:59:02 +0800

    Add IAstroContract methods to AstroServer

--- ServerSoln/Server/AstroServer.cs
+++ ServerSoln/Server/AstroServer.cs
@@ -4,12 +4,30 @@ using System.Linq;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using AstroMath;
 // Kirsten Kurniadi, ID: 30045816
 // Date: 29/08/2022
 // A class that implements the interface
 namespace Server
 {
-    internal class AstroServer
+    internal class AstroServer : IAstroContract
     {
+        Astro astro = new Astro();
+        public double StarVelocity(double d1, double d2)
+        {
+            return astro.StarVelocity(d1, d2);
+        }
+        public double StarDistance(double d)
+        {
+            return astro.StarDistance(d);
+        }
+        public double TempInKelvin(double d)
+        {
+            return astro.TempInKelvin(d);
+        }
+        public double EventHorizon(double d)
+        {
+            return astro.EventHorizon(d);
+        }
     }
 }
