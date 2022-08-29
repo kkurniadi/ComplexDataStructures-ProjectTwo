7748af4d60674eae3557f53b1f133c891e260238
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2022-08-29 10:42:14 +0800

    Added server project files

---
+++ Server/IAstroContract.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+// Kirsten Kurniadi, ID: 30045816
+// Date: 29/08/2022
+// An interface that references the third-party library
+namespace Server
+{
+    public interface IAstroContract
+    {
+    }
+}
