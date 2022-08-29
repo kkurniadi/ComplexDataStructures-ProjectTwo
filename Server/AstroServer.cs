a9459c0e287b60517d1e06beab2f4fd717fb73a9
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2022-08-29 10:45:45 +0800

    Added comment description for client

--- Server/AstroServer.cs
+++ Server/AstroServer.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
-
+// Kirsten Kurniadi, ID: 30045816
+// Date: 29/08/2022
+// A class that implements the interface
 namespace Server
 {
     public class AstroServer
