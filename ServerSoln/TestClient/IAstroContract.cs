53d7adb0e2f63d0e2868d280b1bf38b176b67e67
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2022-08-29 13:41:02 +0800

    Added test client interface

---
+++ ServerSoln/TestClient/IAstroContract.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+// Kirsten Kurniadi, ID: 30045816
+// Date: 29/08/2022
+// A test client interface that does not reference the third-party library
+namespace TestClient
+{
+    [ServiceContract]
+    internal interface IAstroContract
+    {
+    }
+}
