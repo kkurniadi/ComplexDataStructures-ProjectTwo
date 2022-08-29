e6c6e68f9ab8b43070260c111a72704b0d6df3c9
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2022-08-29 12:53:48 +0800

    Added AstroMath library and OperationContract methods

--- ServerSoln/Server/IAstroContract.cs
+++ ServerSoln/Server/IAstroContract.cs
@@ -4,12 +4,22 @@ using System.Linq;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using AstroMath;
 // Kirsten Kurniadi, ID: 30045816
 // Date: 29/08/2022
 // An interface that references the third-party library
 namespace Server
 {
+    [ServiceContract]
     internal interface IAstroContract
     {
+        [OperationContract]
+        double StarVelocity(double d1, double d2);
+        [OperationContract]
+        double StarDistance(double d);
+        [OperationContract]
+        double TempInKelvin(double d);
+        [OperationContract]
+        double EventHorizon(double d);
     }
 }
