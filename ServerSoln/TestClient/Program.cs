09dc452a4173b1a43e701b744a7ad683fd9ba29b
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2022-08-29 13:25:51 +0800

    Add test client project files

---
+++ ServerSoln/TestClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Kirsten Kurniadi, ID: 30045816
+// Date: 29/08/2022
+// Console-based client program for testing purposes
+namespace TestClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
