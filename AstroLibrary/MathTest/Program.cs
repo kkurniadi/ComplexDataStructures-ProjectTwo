39a0373382302a6012d403338c7050a43b66ad49
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2022-08-29 09:26:13 +0800

    Added library console test program files

---
+++ AstroLibrary/MathTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Kirsten Kurniadi, ID: 30045816
+// Date: 29/08/2022
+// A program to test the AstroMath DLL
+namespace MathTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
