63ae1b64dbd98059bec51209df680f8b82d59d70
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2022-09-05 11:25:50 +0800

    Implement switch case for program operation

--- ServerSoln/TestClient/Program.cs
+++ ServerSoln/TestClient/Program.cs
@@ -19,21 +19,47 @@ namespace TestClient
             ChannelFactory<IAstroContract> pipeFactory = new ChannelFactory<IAstroContract>(
                 new NetNamedPipeBinding(), new EndpointAddress("net.pipe://localhost/AstroServer"));
             IAstroContract pipeProxy = pipeFactory.CreateChannel();
+            bool running = true;
 
-            while (true)
+            while (running)
             {
                 Console.WriteLine("To calculate star velocity, enter 'V'");
                 Console.WriteLine("To calculate star distance, enter 'D'");
                 Console.WriteLine("To calculate temperature in Kelvin, enter 'K'");
                 Console.WriteLine("To calculate event horizon, enter 'H'");
-                Console.WriteLine("Enter observed wavelength:");
-                double d1 = double.Parse(Console.ReadLine());
-                Console.WriteLine("Enter rest wavelength:");
-                double d2 = double.Parse(Console.ReadLine());
-                Console.WriteLine("pipe: " + pipeProxy.StarVelocity(d1, d2));
-                Console.WriteLine("Enter parallax angle");
-                double angle = double.Parse(Console.ReadLine());
-                Console.WriteLine("pipe: " + pipeProxy.StarDistance(angle));
+                Console.WriteLine("To exit, enter 'Q'");
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "V":
+                        Console.WriteLine("Enter observed wavelength:");
+                        double d1 = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter rest wavelength:");
+                        double d2 = double.Parse(Console.ReadLine());
+                        Console.WriteLine("pipe: " + pipeProxy.StarVelocity(d1, d2));
+                        break;
+                    case "D":
+                        Console.WriteLine("Enter parallax angle");
+                        double angle = double.Parse(Console.ReadLine());
+                        Console.WriteLine("pipe: " + pipeProxy.StarDistance(angle));
+                        break;
+                    case "K":
+                        Console.WriteLine("Enter temperature in Celsius");
+                        double temp = double.Parse(Console.ReadLine());
+                        Console.WriteLine("pipe: " + pipeProxy.TempInKelvin(temp));
+                        break;
+                    case "H":
+                        Console.WriteLine("Enter blackhole mass");
+                        double mass = double.Parse(Console.ReadLine());
+                        Console.WriteLine("pipe: " + pipeProxy.EventHorizon(mass));
+                        break;
+                    case "Q":
+                        running = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input, try again");
+                        break;
+                }
             }
         }
     }
