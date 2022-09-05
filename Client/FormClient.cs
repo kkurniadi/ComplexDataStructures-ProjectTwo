195984c66b5d444d270d1c7581282379181d2677
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2022-09-05 12:36:11 +0800

    Add OperationContract to IAstroContract

--- Client/FormClient.cs
+++ Client/FormClient.cs
@@ -7,7 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+// Kirsten Kurniadi, ID: 30045816
+// Date: 5/09/2022
+// Windows .NET Framework Forms application client
+// Sends raw data to the server and receives processed information
 namespace Client
 {
     public partial class FormClient : Form
--- Client/IAstroContract.cs
+++ Client/IAstroContract.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
-
+// Kirsten Kurniadi, ID: 30045816
+// Date: 5/09/2022
+// A client interface that does not reference the third-party library
 namespace Client
 {
+    [ServiceContract]
     internal interface IAstroContract
     {
+        [OperationContract]
+        double StarVelocity(double d1, double d2);
+
+        [OperationContract]
+        double StarDistance(double d);
+
+        [OperationContract]
+        double TempInKelvin(double d);
+
+        [OperationContract]
+        double EventHorizon(double d);
     }
 }
