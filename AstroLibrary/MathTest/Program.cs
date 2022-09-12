76a0751161e37b3050bf163a2dee2ccff085770d
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2022-09-12 11:59:13 +0800

    Add MathTest program functionality

--- AstroLibrary/MathTest/Program.cs
+++ AstroLibrary/MathTest/Program.cs
@@ -3,13 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using AstroMath;
+// Kirsten Kurniadi, ID: 30045816
+// Date: 12/09/2022
+// A program to test the AstroMath DLL
 namespace MathTest
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            // Instantiate class library
+            Astro astro = new Astro();
+            // Test the Star Velocity function
+            double sv = astro.StarVelocity(500.1, 500);
+            Console.WriteLine("Star velocity is: " + sv + " m/s");
+            // Test the Star Distance function
+            double sd = astro.StarDistance(0.547);
+            Console.WriteLine("Star distance is " + sd + " parsecs");
+            // Test the Temperature in Kelvin function
+            double k = astro.TempInKelvin(27);
+            Console.WriteLine("Temperature in Kelvin is " + k + " K");
+            // Test the Event Horizon function
+            double mass = 8.2 * Math.Pow(10, 36);
+            double eh = astro.EventHorizon(mass);
+            Console.WriteLine("The event horizon is " + eh + " metres");
+            Console.ReadLine();
         }
     }
 }
